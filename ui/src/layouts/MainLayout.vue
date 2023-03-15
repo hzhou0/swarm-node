719ac72a4677c660372ed5b2b40edc69ce6d1624
Henry <henryzhou000@gmail.com>
2023-03-14 20:53:55 -0400

    Initial ui commit

---
+++ ui/src/layouts/MainLayout.vue
@@ -0,0 +1,55 @@
+<template>
+  <q-layout view="hHh LpR fFf">
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-btn
+          dense
+          flat
+          round
+          icon="menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
+          </q-avatar>
+          Title
+        </q-toolbar-title>
+
+        <q-btn
+          dense
+          flat
+          round
+          icon="menu"
+          @click="rightDrawerOpen = !rightDrawerOpen"
+        />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above side="left" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-drawer v-model="rightDrawerOpen" show-if-above side="right" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>Title </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+  </q-layout>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const leftDrawerOpen = ref(false);
+const rightDrawerOpen = ref(false);
+</script>
