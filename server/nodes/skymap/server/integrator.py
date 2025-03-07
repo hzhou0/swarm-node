a1df6f241765c55dfbce3478a20a60e74c523c77
Henry <henryzhou000@gmail.com>
2025-03-06 22:21:06 -0500

    Add ReconstructionVolume class for 3D point cloud integration
    Implemented a new `integrator.py` module featuring the `ReconstructionVolume` class to manage 3D point cloud optimization, slicing, and file handling with Open3D. Reverted `numpy` dependency from version 2.0.2 to 1.26.4 in `uv.lock` for compatibility with the new implementation.

---
+++ server/nodes/skymap/server/integrator.py
@@ -0,0 +1,306 @@
+import asyncio
+import logging
+import os
+import threading
+import time
+from concurrent.futures import ThreadPoolExecutor
+from pathlib import Path
+
+import numpy as np
+import open3d as o3d
+from numpy.linalg import LinAlgError
+
+
+class Chunk:
+    def __init__(self, pcd: o3d.geometry.PointCloud):
+        self.pcd = pcd
+        self.lock = threading.Lock()
+
+
+class ReconstructionVolume:
+    IMAGE_THRESHOLD = 1000
+    IN_MEMORY_CHUNKS = 3000
+    CHUNK_SIZE = 4
+    VOXEL_SIZE = 0.02
+    #  INTRINSICS = o3d.camera.PinholeCameraIntrinsic(848, 480, 424.770, 424.770, 423.427, 240.898)
+    INTRINSICS = o3d.camera.PinholeCameraIntrinsic(o3d.camera.PrimeSenseDefault)
+
+    def __init__(self, output: Path):
+        self.volume = o3d.pipelines.integration.ScalableTSDFVolume(
+            voxel_length=self.VOXEL_SIZE,
+            sdf_trunc=self.VOXEL_SIZE * 5,
+            color_type=o3d.pipelines.integration.TSDFVolumeColorType.RGB8,
+        )
+        self.num_images = 0
+        self.chunks: dict[tuple[int, int, int], Chunk] = {}
+        self.pcd_queue: asyncio.Queue[o3d.geometry.PointCloud] = asyncio.Queue(maxsize=2)
+        self.active = True
+        self.process_pcd_task_alive = asyncio.Event()
+        self.write_to_disk_task_alive = asyncio.Event()
+        if not output.exists():
+            output.mkdir(exist_ok=True, parents=True)
+        assert output.is_dir()
+        self.output = output
+
+    @classmethod
+    def combine_pcd(
+        cls, source: o3d.geometry.PointCloud, target: o3d.geometry.PointCloud
+    ) -> o3d.geometry.PointCloud:
+        voxel_radius = [cls.VOXEL_SIZE, cls.VOXEL_SIZE / 2, cls.VOXEL_SIZE / 4]
+        max_iter = [50, 30, 14]
+        current_transformation = np.identity(4)
+        result_icp: o3d.pipelines.registration.RegistrationResult | None = None
+        for scale in range(3):
+            i = max_iter[scale]
+            radius = voxel_radius[scale]
+
+            source_down = source.voxel_down_sample(radius)
+            target_down = target.voxel_down_sample(radius)
+
+            source_down.estimate_normals(
+                o3d.geometry.KDTreeSearchParamHybrid(radius=radius * 2, max_nn=30)
+            )
+            target_down.estimate_normals(
+                o3d.geometry.KDTreeSearchParamHybrid(radius=radius * 2, max_nn=30)
+            )
+            try:
+                result_icp: o3d.pipelines.registration.RegistrationResult = (
+                    o3d.pipelines.registration.registration_colored_icp(
+                        source_down,
+                        target_down,
+                        radius,
+                        current_transformation,
+                        o3d.pipelines.registration.TransformationEstimationForColoredICP(),
+                        o3d.pipelines.registration.ICPConvergenceCriteria(
+                            relative_fitness=1e-6, relative_rmse=1e-6, max_iteration=i
+                        ),
+                    )
+                )
+            except Exception as e:
+                break
+
+        target_box: o3d.geometry.AxisAlignedBoundingBox = target.get_axis_aligned_bounding_box()
+        if result_icp and result_icp.fitness > 0.95:
+            source: o3d.geometry.PointCloud = source.transform(result_icp.transformation)
+        source: o3d.geometry.PointCloud = source.crop(target_box)
+        combined: o3d.geometry.PointCloud = source + target
+        return combined.voxel_down_sample(voxel_size=cls.VOXEL_SIZE)
+
+    async def add_image(
+        self, rgbd_image: o3d.geometry.RGBDImage, extrinsic: np.ndarray[np.float64]
+    ):
+        if not self.active:
+            return
+        try:
+            extrinsic = np.linalg.inv(extrinsic)
+        except LinAlgError:
+            logging.error("Image Extrinsic not invertible")
+            return
+        self.volume.integrate(rgbd_image, self.INTRINSICS, extrinsic)
+        self.num_images += 1
+        if self.num_images >= self.IMAGE_THRESHOLD:
+            await self.rollover()
+
+    async def rollover(self):
+        logging.info("reconstructor roll over")
+        start = time.time()
+        pc = self.volume.extract_point_cloud()
+        print(time.time() - start)
+        await self.pcd_queue.put(pc)
+        self.num_images = 0
+        self.volume.reset()
+
+    async def close(self):
+        self.active = False
+        await self.rollover()
+        while self.process_pcd_task_alive.is_set() or self.write_to_disk_task_alive.is_set():
+            await asyncio.sleep(0.1)
+
+    def _slice_point_cloud(self, pc: o3d.geometry.PointCloud):
+        min_box_coords = pc.get_min_bound()
+        max_box_coords = pc.get_max_bound()
+
+        rounded_mins = np.floor(min_box_coords / self.CHUNK_SIZE) * self.CHUNK_SIZE
+        rounded_mins = rounded_mins.astype(np.int64)
+        rounded_maxes = np.ceil(max_box_coords / self.CHUNK_SIZE) * self.CHUNK_SIZE
+        rounded_maxes = rounded_maxes.astype(np.int64)
+
+        logging.debug(f"MIN X: {rounded_mins[0]}, MAX X: {rounded_maxes[0]}")
+
+        def process_sliced_point_cloud(cropped_points: o3d.geometry.PointCloud, _x, _y, _z):
+            cropped_array = np.asarray(cropped_points.points)
+            if not (cropped_array.size and cropped_array.ndim):
+                return
+
+            chunk = self.chunks.get((_x, _y, _z), None)
+            if chunk is not None:
+                with chunk.lock:
+                    chunk.pcd = self.combine_pcd(cropped_points, chunk.pcd)
+                    logging.debug(f"combined in memory {chunk}")
+            else:
+                pcd_file = self._file_for_pcd((_x, _y, _z))
+                try:
+                    if not Path(pcd_file).exists():
+                        raise FileNotFoundError
+                    old_chunk: o3d.geometry.PointCloud = o3d.io.read_point_cloud(pcd_file)
+                    if not old_chunk.has_points():
+                        raise FileNotFoundError
+                    chunk = Chunk(self.combine_pcd(cropped_points, old_chunk))
+                    logging.debug(f"combined from disk {chunk}")
+                except FileNotFoundError:
+                    chunk = Chunk(cropped_points)
+                except Exception as e:
+                    logging.exception(e)
+                # Since the file will either be in memory or is unreadable/nonexistent, always try to remove it
+                try:
+                    os.remove(pcd_file)
+                except FileNotFoundError:
+                    pass
+                self.chunks[(_x, _y, _z)] = chunk
+
+        with ThreadPoolExecutor() as exe:
+            for x in range(rounded_mins[0], rounded_maxes[0], self.CHUNK_SIZE):
+                for y in range(rounded_mins[1], rounded_maxes[1], self.CHUNK_SIZE):
+                    for z in range(rounded_mins[2], rounded_maxes[2], self.CHUNK_SIZE):
+                        box = o3d.geometry.AxisAlignedBoundingBox(
+                            np.array([[x], [y], [z]]),
+                            np.array(
+                                [
+                                    [x + self.CHUNK_SIZE],
+                                    [y + self.CHUNK_SIZE],
+                                    [z + self.CHUNK_SIZE],
+                                ]
+                            ),
+                        )
+                        exe.submit(process_sliced_point_cloud, pc.crop(box), x, y, z)
+        logging.debug("done slicing point cloud")
+
+    async def process_pcd_task(self):
+        if self.process_pcd_task_alive.is_set():
+            return
+        try:
+            self.process_pcd_task_alive.set()
+            while self.active or not self.pcd_queue.empty():
+                try:
+                    pcd = await asyncio.wait_for(self.pcd_queue.get(), 1)
+                except TimeoutError:
+                    continue
+                try:
+                    await asyncio.to_thread(self._slice_point_cloud, pcd)
+                except Exception as e:
+                    logging.exception(e)
+        finally:
+            logging.debug("finish process pcd task")
+            self.process_pcd_task_alive.clear()
+
+    def _file_for_pcd(self, index: tuple[int, int, int]) -> str:
+        return (self.output / f"chunk_{index[0]}_{index[1]}_{index[2]}.pcd").absolute().as_posix()
+
+    async def write_to_disk_task(self):
+        if self.write_to_disk_task_alive.is_set():
+            return
+        try:
+            self.write_to_disk_task_alive.set()
+            while self.active or self.process_pcd_task_alive.is_set() or len(self.chunks.keys()):
+                if (
+                    len(self.chunks.keys()) < self.IN_MEMORY_CHUNKS
+                    and self.process_pcd_task_alive.is_set()
+                ):
+                    await asyncio.sleep(0.1)
+                    continue
+                try:
+                    key, chunk = next(iter(self.chunks.items()))
+                except StopIteration:
+                    await asyncio.sleep(0.1)
+                    continue
+
+                with chunk.lock:
+                    logging.debug(f"writing to disk: {self._file_for_pcd(key)}")
+                    o3d.io.write_point_cloud(
+                        self._file_for_pcd(key),
+                        chunk.pcd,
+                        format="pcd",
+                        write_ascii=False,
+                        compressed=True,
+                    )
+                    del self.chunks[key]
+                await asyncio.sleep(0)
+        finally:
+            logging.debug("finish write to disk task")
+            self.write_to_disk_task_alive.clear()
+
+
+class CameraPose:
+    def __init__(self, meta, mat):
+        self.metadata = meta
+        self.pose = mat
+
+    def __str__(self):
+        return (
+            "Metadata : "
+            + " ".join(map(str, self.metadata))
+            + "\n"
+            + "Pose : "
+            + "\n"
+            + np.array_str(self.pose)
+        )
+
+
+def read_trajectory(filename):
+    traj = []
+    with open(filename, "r") as f:
+        metastr = f.readline()
+        while metastr:
+            metadata = list(map(int, metastr.split()))
+            mat = np.zeros(shape=(4, 4))
+            for i in range(4):
+                matstr = f.readline()
+                mat[i, :] = np.fromstring(matstr, dtype=float, sep=" \t")
+            traj.append(CameraPose(metadata, mat))
+            metastr = f.readline()
+    return traj
+
+
+async def main():
+    logging.basicConfig(level=logging.DEBUG)
+    lounge_rgbd = o3d.data.LoungeRGBDImages()
+    camera_poses = read_trajectory(lounge_rgbd.trajectory_log_path)
+
+    output_dir = Path(__file__).parent / "data"
+    volume = ReconstructionVolume(output_dir)
+    asyncio.create_task(volume.process_pcd_task())
+    asyncio.create_task(volume.write_to_disk_task())
+    for i in range(len(camera_poses) - 1):
+        print("Integrate {:d}-th total image.".format(i))
+        color = o3d.io.read_image(lounge_rgbd.color_paths[i])
+        depth = o3d.io.read_image(lounge_rgbd.depth_paths[i])
+        rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(
+            color, depth, depth_trunc=6.0, convert_rgb_to_intensity=False
+        )
+        await volume.add_image(rgbd, camera_poses[i].pose)
+        await asyncio.sleep(0)
+    await volume.close()
+
+    samples = [o3d.io.read_point_cloud(p) for p in output_dir.glob("*.pcd")]
+    o3d.visualization.draw_geometries(
+        samples,
+        zoom=0.3412,
+        front=[0.4257, -0.2125, -0.8795],
+        lookat=[2.6172, 2.0475, 1.532],
+        up=[-0.0694, -0.9768, 0.2024],
+    )
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+    # def file_for_pcd(output, index: tuple[int, int, int]) -> str:
+    #     return (output / f"chunk_{index[0]}_{index[1]}_{index[2]}.ply").absolute().as_posix()
+
+    # sample = o3d.io.read_point_cloud(file_for_pcd(Path(__file__).parent / "data", (-10, 0, 0)))
+    # o3d.visualization.draw_geometries(
+    #     [sample],
+    #     zoom=0.3412,
+    #     front=[0.4257, -0.2125, -0.8795],
+    #     lookat=[2.6172, 2.0475, 1.532],
+    #     up=[-0.0694, -0.9768, 0.2024],
+    # )
