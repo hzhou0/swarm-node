6ba7a781365f7dbc102f774bcc25c0b354e1742e
Henry <henryzhou000@gmail.com>
2025-03-11 13:06:20 -0400

    test rtcp

--- server/nodes/skymap/common/src/swarmnode_skymap_common/gps.py
+++ server/nodes/skymap/common/src/swarmnode_skymap_common/gps.py
@@ -9,7 +9,7 @@ import msgspec
 import numpy as np
 
 # 848x480 is the most accurate resolution on the D455: https://github.com/IntelRealSense/librealsense/issues/11180
-rgbd_stream_width = 848
+rgbd_stream_width = 640
 rgbd_stream_height = 480
 rgbd_stream_framerate = 5
 macroblock_size = 5
